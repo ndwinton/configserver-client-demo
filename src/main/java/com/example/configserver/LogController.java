432213b6f05e5c8218fb571acf21e633d351f4f3
Neil Winton <nwinton@pivotal.io>
2020-01-30 12:36:30 +0000

    Added refresh capabilities

--- src/main/java/com/example/configserver/LogController.java
+++ src/main/java/com/example/configserver/LogController.java
@@ -3,10 +3,12 @@ package com.example.configserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/log")
 public class LogController {
